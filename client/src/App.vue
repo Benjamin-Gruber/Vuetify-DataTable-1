72cfa49c9264169716ca27f08a00f5f123007a9c
Benjamin-Gruber <83592290+Benjamin-Gruber@users.noreply.github.com>
2021-10-20 18:35:56 +0200

    getMovies

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <v-app>
     <v-app-bar app>
-      <img src="logo.png" width="100" class="mt-10"/><span class="font-weight-black">Movies</span>
-      <v-spacer></v-spacer
-    ></v-app-bar>
+      <img src="logo.png" width="100" class="mt-10"/>
+      <span class="font-weight-black">Movies</span>
+      <v-spacer></v-spacer>
+      <a href="https://www.imdb.com/movies-in-theaters/" style="text-decoration: none ">LATEST REALESES</a>
+    </v-app-bar>
     <v-main>
       <router-view />
     </v-main>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,9 +1,33 @@
-<template>
-  <hello-world />
-</template>
+<template> </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Home',
+
+  data() {
+    return {
+      movies: [],
+    };
+  },
+
+  created() {
+    getMovies;
+  },
+
+  methods: {
+    async getMovies() {
+      try {
+        const { data } = await axios({
+          url: 'http://localhost:3000/cars',
+          method: 'GET',
+        });
+        this.movies = data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
 };
 </script>
